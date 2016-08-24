6b4603514b0e72dcc0742623ab36c44c47335640
kscheltat <kscheltat@outlook.com>
2016-08-24 15:58:54 -0400

    Update quandl.py

--- docs/quandl.py
+++ docs/quandl.py
@@ -1,5 +1,5 @@
-"""Quandl.com is a collection of thousads of finance and economic databases. Please visit Quandl.com for a detailed list of available data.
-This modul provides an API to quandl.com. You can get an api key for free by signing up with quandl.com. You can find the api key 
+"""Quandl.com is a collection of thousands of finance and economic databases. Please visit Quandl.com for a detailed list of available data.
+This module provides an API to quandl.com. You can get an api key for free by signing up with quandl.com. You can find the api key 
 in the account settings.
 """
 from urllib.request import urlopen
